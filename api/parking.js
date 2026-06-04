a9c1ee3739f221e4df52a1228480b103f17f142b
wkc50283-star <wkc50283@gmail.com>
2026-06-04 20:29:15 +0800

    Increase fetch timeout from 10s to 30s

--- api/parking.js
+++ api/parking.js
@@ -22,7 +22,7 @@ module.exports = async function handler(req, res) {
   }
 
   const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), 10000);
+  const timeoutId = setTimeout(() => controller.abort(), 30000);
 
   try {
     const upstream = await fetch(
